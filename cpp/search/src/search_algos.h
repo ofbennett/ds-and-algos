a24882f71093b1bab7331aa006f2e1f1430941fd
ofbennett <ofbennett@gmail.com>
2020-07-04 15:52:04 +0100

    Adds file and dirs for c++ search algos

---
+++ cpp/search/src/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+
+#include <vector>
+#include <iostream>
+
+typedef std::vector<int> vec;
+
+
+
+
+#endif /* SEARCH_ALGOS_H_ */
---
+++ cpp/search/tests/test_search_algos.cpp
@@ -0,0 +1,9 @@
+#include <catch.hpp>
+#include <algorithm> // std::random_shuffle
+#include "search_algos.h"
+
+typedef std::vector<int> vec;
+
+TEST_CASE("This will pass"){
+    REQUIRE(1 == 1);
+}
