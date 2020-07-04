2f256362b2093ebe8df5db21e9a8d924681f5168
ofbennett <ofbennett@gmail.com>
2020-07-04 09:51:08 +0100

    Adds c++ sorting algos file structure

---
+++ cpp/sort/src/sort_algos.h
@@ -0,0 +1,10 @@
+#ifndef SORT_ALGOS_H_
+#define SORT_ALGOS_H_
+
+#include <vector>
+#include <iostream>
+
+
+
+
+#endif /* SORT_ALGOS_H_ */
---
+++ cpp/sort/tests/test_sort_algos.cpp
@@ -0,0 +1,6 @@
+#include <catch.hpp>
+#include "sort_algos.h"
+
+TEST_CASE("Basic test"){
+    REQUIRE(1 == 1);
+}
