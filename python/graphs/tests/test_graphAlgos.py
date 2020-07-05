7e4ebe5ae4637c89d9fc12bf70addac674257be4
ofbennett <ofbennett@gmail.com>
2020-07-05 16:14:54 +0100

    Small refactor of graph algo tests

--- python/graphs/tests/test_graphAlgos.py
+++ python/graphs/tests/test_graphAlgos.py
@@ -2,17 +2,16 @@ import pytest
 from .. import Graph, GraphAdjacencyList, GraphAlgos
 
 @pytest.fixture
-def simpleGraphAdjacencyList():
+def simpleGraph():
     adjacencyList = {0: [1], 1: [0,2], 2: [1]}
     values = {0: 3, 1: 5, 2: 7}
     color = {0: 'red', 1: 'blue', 2: 'green'}
     gal = GraphAdjacencyList(adjacencyList, values = values, color = color)
-    return gal
-
-def test_simpleAlgos(simpleGraphAdjacencyList):
-    gal = simpleGraphAdjacencyList
     graph = Graph()
     graph.buildFromAdjacencyList(gal)
-    gAlgo = GraphAlgos(graph)
+    return graph
+
+def test_simpleAlgos(simpleGraph):
+    gAlgo = GraphAlgos(simpleGraph)
     assert(gAlgo.countNodes() == 3)
     assert(gAlgo.countLinks() == 2)
