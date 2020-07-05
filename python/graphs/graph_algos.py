892fb589833feb5a448695deaf59a799bad3325f
ofbennett <ofbennett@gmail.com>
2020-07-05 17:39:57 +0100

    Adds python graph DFS algos

--- python/graphs/graph_algos.py
+++ python/graphs/graph_algos.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class GraphAlgos:
     def __init__(self, graph):
         self.graph = graph
@@ -17,11 +19,54 @@ class GraphAlgos:
         totalLinks = len(s)
         return totalLinks
     
-    def dfs_recurse(self):
-        pass
+    # Setting testing to True in the search functions below makes the search order unique by sorting each node's children at each stage of the search. Makes testing the functions easier. 
 
-    def dfs_iterative(self):
-        pass
+    def dfsRecurse(self, rootIndex, testing = False):
+        for node in self.graph.nodeArray:
+            node.visited = False
+        visitedNodes = []
+        rootNode = self.graph.nodeArray[rootIndex]
+        self._dfsRecurse(rootNode, visitedNodes, testing)
+        visitedIndexes = [node.index for node in visitedNodes]     
+        for node in self.graph.nodeArray:
+            delattr(node, 'visited')   
+        return visitedIndexes
+    
+    def _dfsRecurse(self, node, visitedNodes, testing = False):
+        if node.visited == False:
+            visitedNodes.append(node)
+            node.visited = True
+        children = node.children
+        if testing:
+            children.sort(key=lambda child: child.index)
+        for child in children:
+            if child.visited == False:
+                self._dfsRecurse(child, visitedNodes, testing)
+        
+
+    def dfsIterative(self, rootIndex, testing = False):
+        for node in self.graph.nodeArray:
+            node.visited = False
+        rootNode = self.graph.nodeArray[rootIndex]
+        visitedNodes = []
+        nodeDeque = deque()
+        nodeDeque.append(rootNode)
+        while len(nodeDeque) != 0:
+            node = nodeDeque.pop()
+            if node.visited == False:
+                visitedNodes.append(node)
+                node.visited = True
+                children = node.children
+                if testing:
+                    children.sort(reverse=True, key=lambda child: child.index)
+                for child in children:
+                    if child.visited == False:
+                        nodeDeque.append(child)
+        visitedIndexes = [node.index for node in visitedNodes]
+        for node in self.graph.nodeArray:
+            delattr(node, 'visited')
+        return visitedIndexes
+
 
     def bfs(self):
         pass
--- python/graphs/tests/test_graphAlgos.py
+++ python/graphs/tests/test_graphAlgos.py
@@ -38,3 +38,13 @@ def test_simpleAlgos(simpleGraph, mediumGraph):
     gAlgo = GraphAlgos(mediumGraph)
     assert(gAlgo.countNodes() == 12)
     assert(gAlgo.countLinks() == 11)
+
+def test_dfs(mediumGraph):
+    gAlgo = GraphAlgos(mediumGraph)
+    correctOrder = [5,4,0,1,2,3,6,7,8,9,10,11]
+    
+    visitedIndexes = gAlgo.dfsRecurse(5, testing = True)
+    assert(visitedIndexes == correctOrder)
+
+    visitedIndexes = gAlgo.dfsIterative(5, testing = True)
+    assert(visitedIndexes == correctOrder)
