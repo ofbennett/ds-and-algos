266f69315a25fd10ff66fb7e2a5bdef31b822439
ofbennett <ofbennett@gmail.com>
2020-07-06 12:02:06 +0100

    Renames variable in python tree algos

--- python/trees/tree_algos.py
+++ python/trees/tree_algos.py
@@ -9,32 +9,35 @@ class BinaryTreeAlgos:
     
     def traverse(self, order = "In-Order"):
         root = self.btree.nodeArray[0]
-        nodeVisitedList = []
-        self._visitNodesRecursively(root, order, nodeVisitedList)
-        nodeVisitedIndexList = [node.index for node in nodeVisitedList]
-        return nodeVisitedIndexList
+        nodesVisitedList = []
+        self._visitNodesRecursively(root, order, nodesVisitedList)
+        nodesVisitedIndexList = [node.index for node in nodesVisitedList]
+        return nodesVisitedIndexList
     
-    def _visitNodesRecursively(self, node, order, nodeVisitedList):
+    def _visitNodesRecursively(self, node, order, nodesVisitedList):
         if order == "In-Order":
             if node.left is not None:
-                self._visitNodesRecursively(node.left, order, nodeVisitedList)
-            nodeVisitedList.append(node)
+                self._visitNodesRecursively(node.left, order, nodesVisitedList)
+            nodesVisitedList.append(node)
             if node.right is not None:
-                self._visitNodesRecursively(node.right, order, nodeVisitedList)
+                self._visitNodesRecursively(node.right, order, nodesVisitedList)
             
         elif order == "Pre-Order":
-            nodeVisitedList.append(node)
+            nodesVisitedList.append(node)
             if node.left is not None:
-                self._visitNodesRecursively(node.left, order, nodeVisitedList)
+                self._visitNodesRecursively(node.left, order, nodesVisitedList)
             if node.right is not None:
-                self._visitNodesRecursively(node.right, order, nodeVisitedList)
+                self._visitNodesRecursively(node.right, order, nodesVisitedList)
             
         elif order == "Post-Order":
             if node.left is not None:
-                self._visitNodesRecursively(node.left, order, nodeVisitedList)
+                self._visitNodesRecursively(node.left, order, nodesVisitedList)
             if node.right is not None:
-                self._visitNodesRecursively(node.right, order, nodeVisitedList)
-            nodeVisitedList.append(node)
-            
+                self._visitNodesRecursively(node.right, order, nodesVisitedList)
+            nodesVisitedList.append(node)
+
         else:
             raise Exception(f"Order string of {order} is invalid. Must be one of [In-Order, Pre-Order, Post-Order]")
+    
+    # def _visitNodesIteratively(self, node, order, nodesVisitedList):
+    #     if order == "In-Order":
